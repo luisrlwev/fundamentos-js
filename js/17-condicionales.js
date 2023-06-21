1427a6159512ca50fffc6449210eaf1071ed9deb
Luis Angel Hernandez <rlwev@hotmail.com>
2023-06-21 17:47:52 -0500

    Clase 17

---
+++ js/17-condicionales.js
@@ -0,0 +1,19 @@
+// Condicionales
+
+const disponible = 4000
+const retirar = 200
+
+if( disponible>retirar){
+    // Se cumple la condición y se ejecuta el código
+    console.log('Si puedes retirar')
+}else{
+    // No se cumple la condición, ejecutar este código
+    console.log('Lo siento, no puedes retirar')
+}
+
+/**
+ * >  Mayor que
+ * <  Menor que
+ * >= Mayor o igual que
+ * <= Menor o igual que
+ */
