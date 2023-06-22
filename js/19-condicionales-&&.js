fed777287667f065ffb9918335fd6dd92463a175
Luis Angel Hernandez <rlwev@hotmail.com>
2023-06-22 16:56:24 -0500

    Clase 19

---
+++ js/19-condicionales-&&.js
@@ -0,0 +1,16 @@
+
+const saldo = 600
+const pagar = 500
+const tarjeta = true
+
+
+/**
+ * || - Al menos una debe cumplirse
+ * && - Todas deben cumplirse
+ */
+
+if(saldo > pagar && tarjeta){
+    console.log('Puedes pagar')
+}else{
+    console.log('No, no puedes pagar')
+}
