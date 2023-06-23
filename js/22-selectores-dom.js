702f4b10c137ed9a631b6abefc3d488a303308a0
Luis Angel Hernandez <rlwev@hotmail.com>
2023-06-23 17:47:42 -0500

    Clase 22

---
+++ js/22-selectores-dom.js
@@ -0,0 +1,8 @@
+// Selectores del DOM
+
+const heading = document.querySelector('.heading')
+
+console.log(heading)
+console.log(heading.textContent)
+console.log(heading.tagName)
+console.log(heading.id)
