44465ba5ef2a0cbc063fc4b1a69c3fd7d8e6595d
Luis Angel Hernandez <rlwev@hotmail.com>
2023-06-28 17:37:21 -0500

    Clase 30

---
+++ js/30-fetch-api-async-await.js
@@ -0,0 +1,10 @@
+// Fetch API - Async Await
+
+const url = "https://jsonplaceholder.typicode.com/comments"
+
+const consultarAPI = async () => {
+    console.log('despues de respuesta')
+    console.log('despues de resultado')
+}
+
+consultarAPI()
