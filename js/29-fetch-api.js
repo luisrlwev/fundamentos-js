d3b1e7da718d7da3cc91e86a524bb9b2aa691221
Luis Angel Hernandez <rlwev@hotmail.com>
2023-06-28 17:19:18 -0500

    Clase 29

---
+++ js/29-fetch-api.js
@@ -0,0 +1,17 @@
+// Fetch API
+
+const url = "https://jsonplaceholder.typicode.com/comments"
+
+const consultarAPI = () => {
+
+    fetch(url)
+        .then( respuesta => respuesta.json())
+        .then( (resultado) => {
+            resultado.forEach(comentario => {
+                console.log(comentario)
+            });
+        })
+
+}
+
+consultarAPI()
